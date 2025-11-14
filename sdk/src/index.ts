ebcde6770608aac7ac795848d280d8b0bc43906d
Solomon Temitayo <temmyjay001@gmail.com>
2025-11-14 02:58:00 +0100

    feat(sdk): Export RetryStrategy from index

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,4 +1,5 @@
 export { FabricX } from './fabricx';
 export * from './types';
 export { Logger, LogLevel } from './utils/logger';
+export { RetryStrategy } from './utils/retry';
 export { GrpcClient } from './grpc/client';
